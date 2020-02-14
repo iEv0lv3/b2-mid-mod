549eed6ff94f8f9cfb0178d78b0f7d64203e640a
Jordan <jordan.wallace.williams@gmail.com>
2020-02-14 11:40:18 -0700

    Add model test for average_age

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -42,4 +42,34 @@ RSpec.describe Movie, type: :model do
       expect(@movie1.young_to_old.last.name).to eq('Jon Boyega')
     end
   end
+
+  describe '#average_age' do
+    it 'should display average age' do
+      @studio1 = Studio.create(name: 'Disney')
+      @studio2 = Studio.create(name: 'MGM')
+
+      @movie1 = @studio1.movies.create(name: 'Star Wars',
+                                        creation_year: '2019',
+                                        genre: 'sci-fi')
+
+      @movie2 = @studio2.movies.create(name: 'Lord of the Rings',
+                                        creation_year: '2006',
+                                        genre: 'fantasy')
+
+      @actor1 = @movie1.actors.create(name: 'Daisy Ridley',
+                                      age: '30')
+
+      @actor2 = @movie1.actors.create(name: 'Jon Boyega',
+                                      age: '32')
+
+      @actor3 = @movie2.actors.create(name: 'Elijah Wood',
+                                      age: '38')
+
+      @actor4 = @movie2.actors.create(name: 'Ian Mcellan',
+                                      age: '75')
+
+      expect(@movie1.average_age).to eq(31)
+      expect(@movie2.average_age).to eq(56)
+    end
+  end
 end

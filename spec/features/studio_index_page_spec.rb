64052900bffea34a9bcd88c9f23d798b086870f5
Jordan <jordan.wallace.williams@gmail.com>
2020-02-14 10:20:09 -0700

    Fix everything. Add creation_year and genre to test setup, fix shouldas

--- spec/features/studio_index_page_spec.rb
+++ spec/features/studio_index_page_spec.rb
@@ -6,13 +6,17 @@ RSpec.describe 'As a visitor', type: :feature do
       @studio1 = Studio.create(name: 'Disney')
       @studio2 = Studio.create(name: 'MGM')
 
-      @movie1 = @studio1.movies.create(name: 'Star Wars')
-      @movie2 = @studio2.movies.create(name: 'Lord of the Rings')
+      @movie1 = @studio1.movies.create(name: 'Star Wars',
+                                       creation_year: '2019',
+                                       genre: 'sci-fi')
 
+      @movie2 = @studio2.movies.create(name: 'Lord of the Rings',
+                                       creation_year: '2006',
+                                       genre: 'fantasy')
     end
+
     it 'I see all of the movie studios' do
       visit '/studios'
-
       within("div#studio_#{@studio1.id}") do
         expect(page).to have_content('Disney')
         expect(page).to have_content('Star Wars')

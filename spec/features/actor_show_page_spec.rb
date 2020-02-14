70157f9ece6f15a10654b4194f2a6bf14acc16a8
Jordan <jordan.wallace.williams@gmail.com>
2020-02-14 10:49:57 -0700

    User story 2 setup and tests

--- spec/features/actor_show_page_spec.rb
+++ spec/features/actor_show_page_spec.rb
@@ -12,17 +12,27 @@ RSpec.describe 'As a visitor', type: :feature do
     @movie2 = @studio2.movies.create(name: 'Lord of the Rings',
                                       creation_year: '2006',
                                       genre: 'fantasy')
+
+    @actor1 = @movie1.actors.create(name: 'Daisy Ridley',
+                                    age: '30')
+
+    @actor2 = @movie2.actors.create(name: 'Elijah Wood',
+                                    age: '38')
   end
 
   describe 'When I visit an actor show page' do
     it 'I see their name, age, and list of all movies they are in' do
+      visit "/actors/#{@actor1.id}"
 
+      expect(page).to have_content('Daisy Ridley')
+      expect(page).to have_content('30')
+      expect(page).to have_content('Star Wars')
+
+      visit "/actors/#{@actor2.id}"
+
+      expect(page).to have_content('Elijah Wood')
+      expect(page).to have_content('Lord of the Rings')
+      expect(page).to have_content('38')
     end
   end
 end
-
-# Story 2
-# As a user,
-# When I visit an actor's show page,
-# I see their name, their age, and a 
-# list of the names of all of the movies they are in.
